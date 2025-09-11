ab144dff2c056fb892eeab121967a3515704126c
John-A-McMahon <108557633+John-A-McMahon@users.noreply.github.com>
2025-09-11 13:28:28 -0500

    Created a proof of concept of what we want.

---
+++ proof_of_concept.py
@@ -0,0 +1,55 @@
+import requests
+import json
+def check_doi(doi):
+    r = requests.get(f"https://api.crossref.org/works/{doi}")
+    return r.status_code == 200
+
+print("enter 'exit' to quit")
+
+
+using = True
+
+users = set()
+
+threads = dict()
+
+while using:
+    user_name=input("Enter username: ")
+    if user_name=="exit":
+        break;
+    if user_name in users:
+        print(f"Welcome back {user_name}")
+    users.add(user_name)
+
+    logged_in = True
+
+    while True:
+        #try 10.1002/jcd.21375
+        chat = input("Enter DOI (form: 10.####/data): ")
+        if chat=="exit":
+            break
+        if check_doi(chat):
+            if chat in threads:
+                print("entered into",chat,"chat")
+            else:
+                print("Chat created for",chat)
+                r = requests.get(f"https://api.crossref.org/works/{chat}")
+                data = r.json()
+                title = (data["message"]["title"])[0]
+                threads[chat]=title
+            print(threads[chat])
+            user_input = input()
+            while user_input!="exit":
+                threads[chat]+=f"\n{user_name}: {user_input}"
+                user_input = input()
+            if user_input=="exit":
+                print(f"Thank you for visiting {chat}!")
+        else:
+            print("invalid DOI or DOI not found")
+
+
+
+
+
+
+
